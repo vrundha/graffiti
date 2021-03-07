ab91ae00610671f924dc4080ea29276166d37cb2
vrundha <vrundha.s@gmail.com>
2021-03-07 14:08:25 -0800

    inserting comments into DB

--- server.py
+++ server.py
@@ -25,6 +25,8 @@ class Graffiti(Resource):
     def __init__(self):
 
         with conn.cursor() as cur:
+            # cur.execute("CREATE USER vru WITH PASSWORD 'cockroach'")
+            # cur.execute("GRANT admin TO vru")
             cur.execute("CREATE DATABASE IF NOT EXISTS social")
             cur.execute("CREATE TABLE IF NOT EXISTS social.graffiti (emotion STRING, comment STRING)")
             conn.commit()
@@ -32,9 +34,19 @@ class Graffiti(Resource):
     def insert(self):
         with conn.cursor() as cur:
             cur.execute("DELETE FROM social.graffiti")
-            rows = [["sad", "My wife is leaving me"], ["sad", "committing suicide"], ["sad", "my husband is leaving me"],
-                    ["sad", "I failed"], ["sad", "I'm getting divorced"],
-                    ["sad", "My wife broke my heart"], ["sad", "My wife is cheating on me!"], ["happy", "I'm married"], ["happy", "I passed!"]]
+            rows = [["sad", "my wife is leaving me"], ["sad", "my husband and I are getting separated"], ["sad", "I'm getting divorced"],
+                    ["sad", "I failed my test"], ["sad", "I got bad scores in my test"],
+                    ["sad", "My wife broke my heart"], ["sad", "My wife is cheating on me!"],
+                    ["sad", "mother passed away due to covid"],
+                    ["happy", "I'm pregnant"], ["happy", "I'm having a baby"],
+                    ["happy", "I won in a hackathon!"], ["happy", "I got first place in a hackathon"],
+                    ["happy", "wonderful trip to malaysia"], ["happy", "I love my family"],
+                    ["happy", "I bought a new car"], ["happy", "I love my car"],
+                    ["calm", "exercising"], ["calm", "taking a nap in my room"], ["calm", "at the sea"], ["calm", "at the mountains"],
+                    ["angry", "so much corruption in the world"], ["angry", "deaths due to covid"], ["angry", "lockdown due to covid"],
+                    ["angry", "can't meet friends due to pandemic"], ["angry", "activities are shut due to covid"],
+                    ["angry", "lockdown"], ["angry", "lockdown"], ["angry","I lost a game"]
+                    ]
             for row in rows:
                 cur.execute("INSERT INTO social.graffiti(emotion, comment) VALUES(%s, %s)", (row[0], row[1]))
             conn.commit()
@@ -53,27 +65,28 @@ class Graffiti(Resource):
         # self.insert()
         _start = time.time()
         with conn.cursor() as cur:
-            print(time.time()-_start)
+            # print(time.time()-_start)
             cur.execute("SELECT emotion, comment FROM social.graffiti")
             rows = cur.fetchall()
             conn.commit()
             comments = {}
             cluster_labels = {}
             frequency = {}
-            print(time.time()-_start, "Starting clustering")
-            for emotion in ["happy", "sad"]:
+            # print(time.time()-_start, "Starting clustering")
+            for emotion in ["happy", "sad", "calm", "angry"]:
                 comments[emotion], cluster_labels[emotion] = semantics.cluster(rows, emotion)
                 frequency[emotion] = self.cluster_counter(comments[emotion], cluster_labels[emotion])
-            print(time.time() - _start, "Stopping clustering")
+            # print(time.time() - _start, "Stopping clustering")
             return frequency
 
         # return rows
 
     def post(self):
         # text = request.form['text']
-        emo = request.form['emo']
+        emo = request.form['emo'].lower()
         comm = request.form['comm']
 
+        # print(emo, comm)
         with conn.cursor() as cur:
             cur.execute("INSERT INTO social.graffiti(emotion, comment) VALUES(%s, %s)", (emo, comm))
             conn.commit()
